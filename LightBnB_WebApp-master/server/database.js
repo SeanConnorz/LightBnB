6a09ae783e94fdac7ff18976c513ecb92dd94bd6
Connor Hughes <seanandhughes@gmail.com>
2021-11-26 00:00:38 +0000

    getAllReservations querry

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -91,7 +91,17 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  const querryString = `
+    SELECT * 
+    FROM reservations
+    WHERE guest_id = $1
+    LIMIT $2;
+  `;
+  const values = [guest_id, limit];
+  return pool
+  .query(querryString, values)
+  .then(result => result.rows)
+  .catch(err => console.log(err.message));
 }
 exports.getAllReservations = getAllReservations;
 
